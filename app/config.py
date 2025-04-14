2e7e049e832290ae444f02a169a6c23ae8437d07
lanlanjr <allanibojr@cspc.edu.ph>
2025-04-14 20:44:56 +0800

    Update database configuration in config.py to use 'aibo$flask_blog' as default MYSQL_DB. Regenerate __pycache__ files for updated modules.

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
     MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'admin'
     MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
-    MYSQL_DB = os.environ.get('MYSQL_DB') or 'flask_blog'
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'aibo$flask_blog'
     
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
